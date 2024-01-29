6688373695e8780b95a573c6ba3ef6b8fd5dbac9
ShotaArima <rin.zoo.mamiya12345@gmail.com>
2024-01-29 22:15:38 +0900

    ひな形完成

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # slack-bot-lab
+
+- handler.jsは今回のメインのファイルでLambdaで実行する処理を書きます
+- serverless.ymlはServerlessFrameworkを使う上での設定ファイルです
+- .gitignoreはgit管理する際にServerlessFrameworkが生成する一時ファイルを管理対象外にするための記載が追加されています
---
+++ app.js
@@ -0,0 +1,84 @@
+const { App, AwsLambdaReceiver } = require('@slack/bolt');
+
+/* 
+This sample slack application uses SocketMode
+For the companion getting started setup guide, 
+see: https://slack.dev/bolt-js/tutorial/getting-started 
+*/
+
+// カスタムのレシーバーを初期化します
+const awsLambdaReceiver = new AwsLambdaReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+// Initializes your app with your bot token and app token
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: awsLambdaReceiver,
+
+  // AwsLambdaReceiver を利用する場合は  `processBeforeResponse` は省略可能です。
+  // OAuth フローに対応した ExpressReceiver など、他のレシーバーを使用する場合、
+  // `processBeforeResponse: true` が必要になります。
+  // このオプションは、ハンドラーの実行が完了するまで応答を返すのを遅延させます。
+  // これによってハンドラーがトリガーとなった HTTP リクエストに応答を返すことでただちに終了されることを防ぐことができます。
+    
+    //processBeforeResponse: true
+
+    // socketMode: true,
+  // appToken: process.env.SLACK_APP_TOKEN
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+// (async () => {
+//   // Start your app
+//   await app.start(process.env.PORT || 3000);
+
+//   console.log('⚡️ Bolt app is running!');
+// })();
+
+// Lambda 関数のイベントを処理します
+module.exports.handler = async (event, context, callback) => {
+  await app.client.chat.postMessage({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: 'C06FLR2DGUX',
+    text: 'Hello world!'
+  });
+  // const handler = await awsLambdaReceiver.start();
+  // return handler(event, context, callback);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'メッセージを送信しました',
+    }),
+  };
+}
