c165c8ebce9bee495c2c7687b9a62b751cf75b30
ShotaArima <g2172010@tcu.ac.jp>
2024-03-17 22:19:40 +0900

    fix script

--- static/slack-bot/public/javascripts/script.js
+++ static/slack-bot/public/javascripts/script.js
@@ -1,15 +1,17 @@
 if (statusCode === 200) {
     alert('ログインに成功しました');
-}else if(statusCode === 401){
-    alert('error message');
-} else if(statusCode === 201){
+} else if (statusCode === 401) {
+    alert('認証エラーが発生しました');
+} else if (statusCode === 201) {
     alert('ユーザーを追加しました');
-}else if(statusCode === 302){
+} else if (statusCode === 302) {
     alert('ユーザーはすでに存在します');
-} else if(statusCode === 500){
+    window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/slack-bot/public/add.html';
+} else if (statusCode === 500) {
     alert('ユーザーの追加中にエラーが発生しました');
-} else if(statusCode === 307){
+} else if (statusCode === 307) {
     alert('メッセージを送信しました');
-} else if(statusCode === 501){
+    window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/logout.html';
+} else if (statusCode === 501) {
     alert('Internal Server Error');
 }
