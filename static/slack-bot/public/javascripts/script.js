0a3bfdbe0549f7cfe01558a1f34d34828fe34358
ShotaArima <g2172010@tcu.ac.jp>
2024-03-17 22:24:46 +0900

    fix script

--- static/slack-bot/public/javascripts/script.js
+++ static/slack-bot/public/javascripts/script.js
@@ -1,17 +1,29 @@
-if (statusCode === 200) {
+if (statusCode === 200) 
+{
     alert('ログインに成功しました');
-} else if (statusCode === 401) {
+    window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/slack-bot/public/main.html';
+} else if (statusCode === 401) 
+{
     alert('認証エラーが発生しました');
-} else if (statusCode === 201) {
+    window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/login.html';
+} else if (statusCode === 201) 
+{
     alert('ユーザーを追加しました');
-} else if (statusCode === 302) {
+    window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/slack-bot/public/login.html';
+} else if (statusCode === 302) 
+{
     alert('ユーザーはすでに存在します');
     window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/slack-bot/public/add.html';
-} else if (statusCode === 500) {
+} else if (statusCode === 500) 
+{
     alert('ユーザーの追加中にエラーが発生しました');
-} else if (statusCode === 307) {
+    window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/slack-bot/public/add.html';
+} else if (statusCode === 307) 
+{
     alert('メッセージを送信しました');
     window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/logout.html';
-} else if (statusCode === 501) {
+} else if (statusCode === 501) 
+{
     alert('Internal Server Error');
+    window.location.href = 'https://slack-bot-real-key.s3.ap-northeast-1.amazonaws.com/slack-bot/public/login.html';
 }
