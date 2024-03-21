80a0087dd4fb8941dab8bdfab37a95a29765915c
ShotaArima <g2172010@tcu.ac.jp>
2024-03-22 03:00:02 +0900

    add console

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ module.exports.handler = async (event, context, callback) => {
               });
               
               if (row) {
+                console.log('row exists.');
                 // ユーザが存在する場合、パスワードのハッシュを比較して認証します
                 const isPasswordValid = await bcrypt.compare(password, row.pass);
 
