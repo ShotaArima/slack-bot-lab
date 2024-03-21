9371f63d501be8c0abbc44486c52a5d377d34a3e
ShotaArima <g2172010@tcu.ac.jp>
2024-03-22 05:37:32 +0900

    fix upload db code

--- app.js
+++ app.js
@@ -227,7 +227,24 @@ module.exports.handler = async (event, context, callback) => {
                 console.log('dbrun complete.');
               }
               // 残りのコード（ユーザー数の取得、データベースの閉じる、変更の反映など）
-          
+              // 元のデータベースファイルと一時的なデータベースファイルを比較して変更が必要かどうかを確認
+              const originalDatabaseContent = fs.readFileSync(download_path2, 'utf-8');
+              const newDatabaseContent = fs.readFileSync(download_path, 'utf-8');
+              if (originalDatabaseContent !== newDatabaseContent) {
+                // 変更がある場合のみ元のデータベースファイルに変更を適用
+                fs.copyFileSync(download_path, 'db/slack.db');
+                console.log('変更が元のデータベースに反映されました');
+              } else {
+                console.log('変更は不要です');
+              }
+              conn.close((err) => {
+                if (err) {
+                  console.error('Error closing database connection:', err);
+                  return;
+                }
+                console.log('Connection closed');
+              });
+
               return callback(null, {
                 statusCode: 302,
                 headers: {
@@ -310,23 +327,6 @@ module.exports.handler = async (event, context, callback) => {
           }
 
 
-        // 元のデータベースファイルと一時的なデータベースファイルを比較して変更が必要かどうかを確認
-        const originalDatabaseContent = fs.readFileSync(download_path2, 'utf-8');
-        const newDatabaseContent = fs.readFileSync(download_path, 'utf-8');
-        if (originalDatabaseContent !== newDatabaseContent) {
-          // 変更がある場合のみ元のデータベースファイルに変更を適用
-          fs.copyFileSync(download_path, 'db/slack.db');
-          console.log('変更が元のデータベースに反映されました');
-        } else {
-          console.log('変更は不要です');
-        }
-        conn.close((err) => {
-          if (err) {
-            console.error('Error closing database connection:', err);
-            return;
-          }
-          console.log('Connection closed');
-        });
     } catch (error) {
       console.error('Error downloading database from S3', error);
       console.error(error);
